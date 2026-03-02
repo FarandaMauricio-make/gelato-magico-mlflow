352f6677b4bbb9999cd6b34cc4ad715a787e4a30
FarandaMauricio-make <fabricio.miranda.lvs@gmail.com>
2026-03-01 22:57:15 -0300

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,1 +1,18 @@
+import subprocess
 
+def run():
+    print("1) Rodando treinamento com MLflow...")
+    subprocess.run(["python", "src/train.py"], check=True)
+
+    print("\n2) Testando previsão rápida...")
+    # exemplo com 30°C
+    p = subprocess.run(
+        ["python", "src/predict.py"],
+        input="30\n",
+        text=True,
+        capture_output=True
+    )
+    print(p.stdout)
+
+if __name__ == "__main__":
+    run()
